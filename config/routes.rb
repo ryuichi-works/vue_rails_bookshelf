72ca96408c76d2f87c812f1074fe576c3bbceab0
ryuIM <songyeweil@gmail.com>
2021-04-23 17:16:17 +0900

    add: Booksコントローラを生成し，本情報を表示まで #1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+
+  get 'books/index'
   root to: 'home#index'
+  resources :books, only: [:index]
   # get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
