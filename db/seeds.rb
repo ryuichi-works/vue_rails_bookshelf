79e2823fc22e0aaee9a9bfe3ed0d05ec42ba0135
ryuIM <songyeweil@gmail.com>
2021-05-08 00:02:24 +0900

    add: Backend(Rails側)/サンプルデータを追加まで #9

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,13 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.create!(
+  name: 'Admin User',
+  email: 'example@example.com',
+  password: 'foobar',
+  password_confirmation: 'foobar'
+)
+
 20.times do
   Book.create!(
     title: Faker::Book.title,
